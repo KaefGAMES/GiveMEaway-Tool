984d2fe8512c9e405fedb43ffbc4047bc49459f0
D3thw0lf <deathwolf@kaefgames.de>
2015-10-14 07:18:03 +0200

    more comments...geez!

--- GiveMEaway/Form1.vb
+++ GiveMEaway/Form1.vb
@@ -50,6 +50,9 @@
     Private Const versionURL = "http://www.kaefgames.de/gaway/current_ver.txt"
 
     Dim Web As New Net.WebClient()
+    '
+    ' Pingt google.de vor dem Update weil Google eigentlich IMMER erreichbar ist, wenn man Internet hat!
+    '
     Function TestInternetConnection()
 
         Dim ping As New Net.NetworkInformation.Ping
@@ -61,6 +64,10 @@
             Return True
         End Try
     End Function
+    ' Testet vor dem Start des Updates dank der Funktion "TestInternetConnection()" ob Internet verfügbar ist
+    ' Beginnt dann damit die Versionsnummer vom Webserver auszulesen und wenn diese größer ist als unsere jetzige,
+    ' in der Software gespeicherte, Versionsnummer dann startet die geplante GiveMEupdate.exe während sich
+    ' GiveMEaway.exe für den Updateprozess schließt um diesen nicht zu verhindern. Easy Peasy!
     Sub RunUpdate()
         If TestInternetConnection() = True Then
             Try
@@ -76,18 +83,20 @@
         End If
     End Sub
     '
-    '
+    ' Fügt eine neue Zeile (temporär) zur Liste hinzu. Das Ziel hier ist es, die Zeile später in die keys.txt zu schreiben...
     '
     Private Sub addkey_Click(sender As Object, e As EventArgs) Handles addkey.Click
         aviablekeys.Text &= Environment.NewLine + "" + "+" + "" + " " + " " & keybox.Text
     End Sub
     '
-    '
+    ' Spielt den mitgelieferten Sound ab. Sounddatei kann und sollte angepasst werden...es sei denn man mag den Sound! Dann sollte hier nix geändert werden. ;)
     '
     Private Sub playsound_Click(sender As Object, e As EventArgs) Handles playsound.Click
         My.Computer.Audio.Play(System.Environment.CurrentDirectory + "\Sounds\justfun\default.wav", AudioPlayMode.Background)
     End Sub
-
+    '
+    ' Steht ja da! Löscht allerdings später die Keys aus der keys.txt
+    '
     Private Sub remkey_Click(sender As Object, e As EventArgs) Handles remkey.Click
         aviablekeys.Text &= Environment.NewLine + "#" + "#" + "#" & "NOT YET AVIABLE" + "#" + "#" + "#"
     End Sub

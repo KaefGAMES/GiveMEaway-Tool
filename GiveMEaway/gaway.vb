946bd0a419c7d9ecb9e77b8a54358eba7b8b6b7c
D3thw0lf <deathwolf@kaefgames.de>
2015-10-20 00:30:02 +0200

    update changelog

--- GiveMEaway/gaway.vb
+++ GiveMEaway/gaway.vb
@@ -59,7 +59,7 @@
         '
         ' Startet normal weiter, wenn keine Updates vorhanden sind
         '
-        MsgBox("Changelog #6" & vbNewLine & "" & vbNewLine & "- Die GiveMEupdate.exe ist vorläufig nicht mehr dabei! Sollte eine ältere Version in deinem Verzeichnis liegen, lösche diese bitte." & vbNewLine & "" & vbNewLine & "- Ein Button zum Kopieren des jeweils ausgewählten Keys ist jetzt verfügbar.", MsgBoxStyle.Information, "GiveMEAway - Willkommen! :)")
+        MsgBox("Changelog #6" & vbNewLine & "" & vbNewLine & "- Die GiveMEupdate.exe ist vorläufig nicht mehr dabei! Sollte eine ältere Version in deinem Verzeichnis liegen, wird diese automatisch gelöscht." & vbNewLine & "" & vbNewLine & "- Ein Button zum Kopieren des jeweils ausgewählten Keys ist jetzt verfügbar.", MsgBoxStyle.Information, "GiveMEAway - Willkommen! :)")
         MsgBox("Vielen Dank, dass du dich für unser kleines aber feines Tool zum Verwalten deiner GiveAway Keys entschieden hast." & vbNewLine & "" & vbNewLine & "Dieses Tool soll dich dabei unterstützen eine Liste von deinen Keys anzufertigen, für deinen aktuellen Livestream. Das Tool befindet sich noch in Entwicklung und Feedback ist gerne gesehen." & vbNewLine & "" & vbNewLine & "Besuche unsere Webseite für mehr Infos!" & vbNewLine & "www.kaefgames.de", MsgBoxStyle.Information, "GiveMEAway - Willkommen! :)")
         '
         ' Bereitet das Laden der keys.txt als Liste vor

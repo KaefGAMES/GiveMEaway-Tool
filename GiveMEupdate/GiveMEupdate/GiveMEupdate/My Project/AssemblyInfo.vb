f22de790a0c29ca0e9fd984523930297786f500a
D3thw0lf <deathwolf@kaefgames.de>
2015-10-31 16:46:01 +0100

    added GiveMEupdate source

---
+++ GiveMEupdate/GiveMEupdate/GiveMEupdate/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("GiveMEupdate")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("KaefGAMES-Entertainment")>
+<Assembly: AssemblyProduct("GiveMEupdate")>
+<Assembly: AssemblyCopyright("Copyright ©  2015 by KaefGAMES-Entertainment")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("73ff7e99-55d9-457e-b014-b0d987ae6945")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("0.0.6.1")>

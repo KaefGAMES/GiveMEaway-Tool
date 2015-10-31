ab8a4339de788ebf7b3e31c908a895448461f50f
D3thw0lf <deathwolf@kaefgames.de>
2015-10-31 17:29:36 +0100

    fixed multiline not working when the key starts with a letter instead of a number (for example: E12345K-7C9-0 would work now) now the empty-check is missing (caused the mistake) but anyways..I'll do that later (just don't leave 'em empty and you won't get any empty lines)

--- GiveMEaway/Mkey.Designer.vb
+++ GiveMEaway/Mkey.Designer.vb
@@ -23,49 +23,14 @@ Partial Class Mkey
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Mkey))
-        Me.key1 = New System.Windows.Forms.TextBox()
-        Me.key2 = New System.Windows.Forms.TextBox()
-        Me.key4 = New System.Windows.Forms.TextBox()
-        Me.key3 = New System.Windows.Forms.TextBox()
-        Me.key5 = New System.Windows.Forms.TextBox()
         Me.Button1 = New System.Windows.Forms.Button()
         Me.Label1 = New System.Windows.Forms.Label()
+        Me.Key1 = New System.Windows.Forms.TextBox()
+        Me.key2 = New System.Windows.Forms.TextBox()
+        Me.key3 = New System.Windows.Forms.TextBox()
+        Me.key4 = New System.Windows.Forms.TextBox()
+        Me.key5 = New System.Windows.Forms.TextBox()
         Me.SuspendLayout()
-        '
-        'key1
-        '
-        Me.key1.Location = New System.Drawing.Point(12, 53)
-        Me.key1.Name = "key1"
-        Me.key1.Size = New System.Drawing.Size(341, 20)
-        Me.key1.TabIndex = 0
-        '
-        'key2
-        '
-        Me.key2.Location = New System.Drawing.Point(12, 79)
-        Me.key2.Name = "key2"
-        Me.key2.Size = New System.Drawing.Size(341, 20)
-        Me.key2.TabIndex = 1
-        '
-        'key4
-        '
-        Me.key4.Location = New System.Drawing.Point(12, 131)
-        Me.key4.Name = "key4"
-        Me.key4.Size = New System.Drawing.Size(341, 20)
-        Me.key4.TabIndex = 3
-        '
-        'key3
-        '
-        Me.key3.Location = New System.Drawing.Point(12, 105)
-        Me.key3.Name = "key3"
-        Me.key3.Size = New System.Drawing.Size(341, 20)
-        Me.key3.TabIndex = 2
-        '
-        'key5
-        '
-        Me.key5.Location = New System.Drawing.Point(12, 157)
-        Me.key5.Name = "key5"
-        Me.key5.Size = New System.Drawing.Size(341, 20)
-        Me.key5.TabIndex = 4
         '
         'Button1
         '
@@ -84,22 +49,57 @@ Partial Class Mkey
         Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label1.Location = New System.Drawing.Point(8, 18)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(221, 24)
+        Me.Label1.Size = New System.Drawing.Size(227, 24)
         Me.Label1.TabIndex = 6
-        Me.Label1.Text = "Füge bis zu 5 Keys hinzu"
+        Me.Label1.Text = "Mehrere Keys hinzufügen"
+        '
+        'Key1
+        '
+        Me.Key1.Location = New System.Drawing.Point(12, 46)
+        Me.Key1.Name = "Key1"
+        Me.Key1.Size = New System.Drawing.Size(341, 20)
+        Me.Key1.TabIndex = 7
+        '
+        'key2
+        '
+        Me.key2.Location = New System.Drawing.Point(12, 72)
+        Me.key2.Name = "key2"
+        Me.key2.Size = New System.Drawing.Size(341, 20)
+        Me.key2.TabIndex = 8
+        '
+        'key3
+        '
+        Me.key3.Location = New System.Drawing.Point(12, 98)
+        Me.key3.Name = "key3"
+        Me.key3.Size = New System.Drawing.Size(341, 20)
+        Me.key3.TabIndex = 9
+        '
+        'key4
+        '
+        Me.key4.Location = New System.Drawing.Point(12, 124)
+        Me.key4.Name = "key4"
+        Me.key4.Size = New System.Drawing.Size(341, 20)
+        Me.key4.TabIndex = 10
+        '
+        'key5
+        '
+        Me.key5.Location = New System.Drawing.Point(12, 150)
+        Me.key5.Name = "key5"
+        Me.key5.Size = New System.Drawing.Size(341, 20)
+        Me.key5.TabIndex = 11
         '
         'Mkey
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(365, 243)
-        Me.Controls.Add(Me.Label1)
-        Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.key5)
         Me.Controls.Add(Me.key4)
         Me.Controls.Add(Me.key3)
         Me.Controls.Add(Me.key2)
-        Me.Controls.Add(Me.key1)
+        Me.Controls.Add(Me.Key1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button1)
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.MaximizeBox = False
         Me.MaximumSize = New System.Drawing.Size(381, 282)
@@ -112,12 +112,11 @@ Partial Class Mkey
         Me.PerformLayout()
 
     End Sub
-
-    Friend WithEvents key1 As TextBox
-    Friend WithEvents key2 As TextBox
-    Friend WithEvents key4 As TextBox
-    Friend WithEvents key3 As TextBox
-    Friend WithEvents key5 As TextBox
     Friend WithEvents Button1 As Button
     Friend WithEvents Label1 As Label
+    Friend WithEvents Key1 As TextBox
+    Friend WithEvents key2 As TextBox
+    Friend WithEvents key3 As TextBox
+    Friend WithEvents key4 As TextBox
+    Friend WithEvents key5 As TextBox
 End Class
